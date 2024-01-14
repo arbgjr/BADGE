832027d830064ce10fb87dd887ba2f479b4dc32c
Armando Guimarães Junior <armando.rogerio.jr@gmail.com>
2024-01-13 23:57:41 -0300

    Update helpers.py

--- Badge/helpers.py
+++ Badge/helpers.py
@@ -38,7 +38,7 @@ try:
 
     key_vault_url = client.get_configuration_setting("AzKVURI")
     if not key_vault_url:
-        raise ValueError(f"Configuração para a chave '{key}' não encontrada.")
+        raise ValueError("Configuração para a chave 'AzKVURI' não encontrada.")
      
     secret_client = SecretClient(vault_url=key_vault_url, credential=credential)
 
