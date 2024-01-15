747c97129a7969335c1ed706e8ec9a41426daae2
Armando Guimarães Junior <armando.rogerio.jr@gmail.com>
2024-01-15 16:42:05 -0300

    On branch dev  Your branch is up to date with 'origin/dev'.
     Changes to be committed:
    	modified:   Badge/__init__.py
    	modified:   Badge/database.py
    
    acertando database e iserindo tratameto de erro geral.

--- Badge/__init__.py
+++ Badge/__init__.py
@@ -1,11 +1,34 @@
 import traceback
 import azure.functions as func
+from flask import jsonify
 from .logger import LogLevel, Logger
 
 # Crie uma instância do Logger com um nome específico
 logger = Logger("AzFuncBadges", default_level=LogLevel.DEBUG)
 
 from .app import application
+
+def format_exception(e):
+    trace = traceback.format_exception(etype=type(e), value=e, tb=e.__traceback__)
+
+    formatted_trace = []
+    for line in trace:
+        if " File" in line or " ^" in line:
+            formatted_trace.append("\n" + line.strip())
+        else:
+            formatted_trace.append(line.strip())
+
+    pretty_exception = "".join(formatted_trace)
+
+    return pretty_exception
+
+@app.errorhandler(Exception)
+def handle_exception(e):
+    formatted_error = format_exception(e)
+    response = jsonify(message=str(e), formatted_error=formatted_error, type=type(e).__name__)
+    response.status_code = 500
+    return response
+
 
 def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
     # Log da solicitação recebida
--- Badge/database.py
+++ Badge/database.py
@@ -6,7 +6,7 @@ from . import azure
 from . import logger, LogLevel
 
 class Database:
-    def __init__(self, logger_name):
+    def __init__(self):
         self.logger = logger
 
         # Configuração do cliente Azure
