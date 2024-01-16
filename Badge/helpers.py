d4b845189dd61cb9f5710bac268955f34176a6f5
Armando Guimarães Junior <armando.rogerio.jr@gmail.com>
2024-01-16 02:09:45 -0300

    On branch dev  Your branch is up to date with 'origin/dev'.
     Changes to be committed:
    	modified:   Badge/helpers.py

--- Badge/helpers.py
+++ Badge/helpers.py
@@ -47,20 +47,23 @@ def decrypt_data(encrypted_data):
 def encrypt_data(data):
     gpg = gnupg.GPG()
     public_key = get_pgp_public_key()
+    logger.log(LogLevel.DEBUG, f"Public Key: {public_key}")
+
+    # Importar a chave pública PGP
+    logger.log(LogLevel.DEBUG, f"Importando a chave pública PGP:")
     import_result = gpg.import_keys(public_key)
 
-    # Verifique se pelo menos uma chave foi importada usando 'counts' ou 'fingerprints'
-    logger.log(LogLevel.DEBUG, f"Verifique se pelo menos uma chave foi importada usando 'counts' ou 'fingerprints'")
+    # Verifique se a importação foi bem-sucedida
     if not import_result.counts['imported'] and not import_result.fingerprints:
+        # Imprimir informações detalhadas do resultado da importação
+        logger.log(LogLevel.DEBUG, f"Detalhes da Importação:")
+        logger.log(LogLevel.DEBUG, f"Contagem de importados: {import_result.counts['imported']}")
+        logger.log(LogLevel.DEBUG, f"Contagem de não alterados: {import_result.counts['unchanged']}")
+        logger.log(LogLevel.DEBUG, f"Resultados da importação: {import_result.results}")
+        logger.log(LogLevel.DEBUG, f"Stderr: {import_result.stderr}")
         raise ValueError("Falha ao importar a chave pública PGP")
 
-    # Usa a primeira fingerprint importada para a criptografia
-    logger.log(LogLevel.DEBUG, f"Usa a primeira fingerprint importada para a criptografia")
-    fingerprint = import_result.fingerprints[0] if import_result.fingerprints else None
-    if not fingerprint:
-        raise ValueError("Nenhuma fingerprint válida encontrada após a importação da chave")
-
-    encrypted_data = gpg.encrypt(data, fingerprint)
+    encrypted_data = gpg.encrypt(data, import_result.fingerprints[0])
     if not encrypted_data.ok:
         raise ValueError(f"Falha na criptografia: {encrypted_data.status}")
     
